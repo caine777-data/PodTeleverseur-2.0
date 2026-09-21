339b6270115741daedfb5f2938f4dea273e1afc5
caine777-data <caine777@hotmail.fr>
2026-09-21 19:55:39 +0200

    update_obligatoire

--- __version__.py
+++ __version__.py
@@ -18,7 +18,7 @@ jour lors d'une publication : il ne peut pas importer de code Python. Un test
 vérifie qu'il concorde.
 """
 
-__version__ = "2.2.0"
+__version__ = "2.3.0"
 
 # Décomposition (entiers), pour les métadonnées Windows :
 # (majeur, mineur, correctif, build).
